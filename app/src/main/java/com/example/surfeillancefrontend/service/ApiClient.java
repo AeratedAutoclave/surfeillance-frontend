c3c662825e7d7a89dd4b9b2d4e651b92a4ba3b40
tomfegan <tomfegan83@gmail.com>
2024-07-08 15:45:05 +0100

    update: adding files

--- app/src/main/java/com/example/surfeillancefrontend/service/ApiClient.java
+++ app/src/main/java/com/example/surfeillancefrontend/service/ApiClient.java
@@ -12,13 +12,12 @@ public class ApiClient {
         if (retrofit == null) {
 
             // When adding headers like authentication tokens, ensure they are securely stored
-            // and not hard-coded directly into your application. Consider using environment variables or
-            // secure storage solutions for such sensitive information
+            // and not hard-coded directly into your application. Consider using environment
+            // variables or secure storage solutions for such sensitive information
             OkHttpClient client = new OkHttpClient.Builder()
 //                    .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)) // Optional: For logging
-                    .addInterceptor( new HeaderInterceptor("Your-Header-Key", "Your-Header-Value")) // Add your custom header here
+                    .addInterceptor(new HeaderInterceptor("Your-Header-Key", "Your-Header-Value")) // Add your custom header here
                     .build();
-
 
             retrofit = new Retrofit.Builder()
                     .baseUrl("http://10.0.2.2:8080/")
